f181bcf41397f035f7034e1787ac94b6434355c7
asadhayatt <asadhkan@gmail.com>
2022-05-10 17:53:27 +0500

    10-05-2022 Project update

--- app/src/main/java/com/hayat/stockprobeta/Dashboard2.java
+++ app/src/main/java/com/hayat/stockprobeta/Dashboard2.java
@@ -7,24 +7,17 @@ import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Locale;
-import java.util.SimpleTimeZone;
 
 public class Dashboard2 extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
@@ -171,7 +164,7 @@ public class Dashboard2 extends AppCompatActivity implements DatePickerDialog.On
         reportlist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),SalelistActivity.class));
+                startActivity(new Intent(getApplicationContext(), ReportActivity.class));
             }
         });
         loadExpense();
